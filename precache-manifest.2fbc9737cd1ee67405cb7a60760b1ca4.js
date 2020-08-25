801ecce42f68bf765d6986fd23566a93ddce99c1
Vyacheslav <jantschuk@gmail.com>
2020-08-25 12:01:55 +0300

    init

---
+++ precache-manifest.2fbc9737cd1ee67405cb7a60760b1ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727debc10239c0fafc4e6ce69db5e7cb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2364dde1686510adef67",
+    "url": "./static/css/main.15fb82cc.chunk.css"
+  },
+  {
+    "revision": "b758a909d155ea1a276d",
+    "url": "./static/js/2.0fd1a4ee.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "./static/js/2.0fd1a4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2364dde1686510adef67",
+    "url": "./static/js/main.49d655b1.chunk.js"
+  },
+  {
+    "revision": "fa893815ae0ad15479da",
+    "url": "./static/js/runtime-main.6ad1334e.js"
+  }
+]);
